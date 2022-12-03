cd244b632411da19b9c57cab489cdd35f8ce908c
Rintaro Itokawa <me@re-taro.dev>
2022-12-03 18:30:41 +0900

    chore: Fix npm scripts

---
+++ test/fixture/react.tsx
@@ -0,0 +1,12 @@
+import type { FC } from 'react'
+
+const Component: FC = () => {
+    const click = () => {}
+    return (
+        <button onClick={click} onBlur={() => {}} disabled>
+            <p>hi</p> |
+        </button>
+    )
+}
+
+export default Component
