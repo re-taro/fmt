de14d8411d9c1ef94f5637cca36d91cc0975b172
Rintaro Itokawa <me@re-taro.dev>
2023-10-26 08:47:38 +0900

    chore: remove unsupported eslintignore

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,5 @@
 import { re_taro } from "@re-taro/eslint-config";
 
 export default re_taro({
-  gitignore: {
-    files: [".gitignore", ".eslintignore"],
-  },
+  ignores: ["**/rollup.config.mjs"],
 });
