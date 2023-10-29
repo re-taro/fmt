dad29bab86c578017622ca6e8db09b7baf816265
Rintaro Itokawa <me@re-taro.dev>
2023-10-29 12:49:28 +0900

    fix: add parserOptions props

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,12 @@
 import { re_taro } from "@re-taro/eslint-config";
 
-export default re_taro({
-  ignores: ["**/rollup.config.mjs"],
-});
+export default re_taro(
+  {
+    parserOptions: {
+      project: "./tsconfig.json",
+    },
+  },
+  {
+    ignores: ["**/rollup.config.mjs"],
+  },
+);
