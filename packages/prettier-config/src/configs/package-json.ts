5b51903074ad6b3a446862a66eee18a5645fc0bb
Rintaro Itokawa <me@re-taro.dev>
2024-07-28 01:29:54 +0900

    refactor: drop prettier config

--- packages/prettier-config/src/configs/package-json.ts
+++
@@ -1,8 +0,0 @@
-import type { Config } from "../types";
-import { require } from "../utils";
-
-export function packageJson(): Config {
-	return {
-		plugins: [require.resolve("prettier-plugin-pkgsort")],
-	};
-}
