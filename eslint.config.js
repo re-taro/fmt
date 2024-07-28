1d98a8cd0d2d15358a01506138ca0223f0cb1afa
Rintaro Itokawa <me@re-taro.dev>
2024-07-28 18:54:54 +0900

    build: use isolatedDecralation

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,40 @@
-// @ts-check
+// @ts-expect-error missing types
+import styleMigrate from '@stylistic/eslint-plugin-migrate'
+import { tsImport } from 'tsx/esm/api'
 
-import { re_taro } from "@re-taro/eslint-config";
+/** @type {typeof import('./packages/eslint-config/src/index.ts')} */
+const { re_taro } = await tsImport("./packages/eslint-config/src/index.ts", import.meta.url)
 
-export default re_taro({
-	typescript: true,
-	formatters: true,
-	ignores: ["**/vendor/**"],
-}, {
-	files: ["**/src/**/*.ts"],
-	rules: {
-		"perfectionist/sort-objects": "error",
-	},
-});
+export default re_taro(
+	{
+    vue: true,
+    react: true,
+    solid: true,
+    svelte: true,
+    astro: true,
+    typescript: true,
+    formatters: true,
+    type: 'lib',
+  },
+  {
+    ignores: [
+      'packages/eslint-config/fixtures',
+      'packages/eslint-config/_fixtures',
+    ],
+  },
+  {
+    files: ['packages/eslint-config/src/**/*.ts'],
+    rules: {
+      'perfectionist/sort-objects': 'error',
+    },
+  },
+  {
+    files: ['packages/eslint-config/src/configs/*.ts'],
+    plugins: {
+      'style-migrate': styleMigrate,
+    },
+    rules: {
+      'style-migrate/migrate': ['error', { namespaceTo: 'style' }],
+    },
+  },
+);
